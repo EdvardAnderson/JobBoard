186e73e4313404c4be343ffc0013253ac4fe6c51
Edvard Anderson <php078@gmail.com>
2024-07-15 09:49:04 +0200

    created entities files

---
+++ JobBoard.API/Entities/Company.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JobBoard.API.Entities;
+
+public class Company
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    public string Address { get; set; }
+
+    public ICollection<JobOpening> JobOpenings { get; set; } = new List<JobOpening>();
+}
---
+++ JobBoard.API/Entities/JobOpening.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JobBoard.API.Entities;
+
+public class JobOpening
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Title { get; set; }
+
+    [Required]
+    [MaxLength(4000)]
+    public string Description { get; set; }
+
+    public bool IsActive { get; set; }
+
+    [ForeignKey("CompanyId")]
+    public Company Company { get; set; }
+    public Guid CompanyId { get; set; }
+}
